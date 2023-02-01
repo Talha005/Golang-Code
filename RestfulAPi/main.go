f6f414fc2adc8fae09b056f0155cf7879cb4b897
Muhammad Talha <Talha.0801@live.com>
2023-02-01 12:29:07 +0500

    comit

--- RestfulAPi/main.go
+++ RestfulAPi/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+//Model for course - file
+
+type Course struct {
+	CourseId    string `json:"courseid"`
+	CourseName  string `json:"coursename"`
+	CoursePrice int    `json:"price"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
 func main() {
 
 	fmt.Println("Hello")
